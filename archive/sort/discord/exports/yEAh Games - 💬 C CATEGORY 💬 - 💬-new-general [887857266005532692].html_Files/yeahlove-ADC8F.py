a84d190457ca6055a11699ff550b68b6ce10b390
yEAh-Games <tallinnquinton@gmail.com>
2022-12-25 19:58:15 -0500

    part 4

---
+++ archive/sort/discord/exports/yEAh Games - 💬 C CATEGORY 💬 - 💬-new-general [887857266005532692].html_Files/yeahlove-ADC8F.py
@@ -0,0 +1,37 @@
+676	-     if message.content.startswith('yeahlove'):
+677	-         embedVar = discord.Embed(
+678	-             title="yEAh | Love",
+679	-             description="**DISCLAIMER: THIS COMMAND IS PURELY FOR HUMOUROUS PURPOSES!** View all the known relationships in our class(es), along with what they're called, and some additional info.", color=0xff4747)
+680	-         embedVar.set_author(
+681	-             name="School-related 📚 | yEAh Games ",
+682	-             url="https://discord.gg/GNnPQ2h5",
+683	-             icon_url=
+684	-             "")
+685	-         embedVar.add_field(name="**David ❤️ Emalea**",
+686	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *David was found stroking and rubbing—to keep this a G-area—Emalea's desk at school.*",
+687	-                            inline=False)
+688	-         embedVar.add_field(name="**Becca ❤️ Tallinn**",
+689	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *Anonymous B..h-person said so.*",
+690	-                            inline=False)
+691	-         embedVar.add_field(name="**David ❤️ Marieke**",
+692	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *David is usually blushing when talking to Marieke.*",
+693	-                            inline=False)
+694	-         embedVar.add_field(name="**Marieke ❤️ Aidan**",
+695	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *Marieke is usually blushing, and/or acting strange near Aidan.*",
+696	-                            inline=False)
+697	-         embedVar.add_field(name="**Becca ❤️ Aidan**",
+698	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *Becca has said that she “likes Aidan.” She has also said this to other people, though.*",
+699	-                            inline=False)
+700	-         embedVar.add_field(name="**Andrew ❤️ Ethan**",
+701	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *Andrew was found doing some (questionable) things to Ethan.*",
+702	-                            inline=False)
+703	-         embedVar.add_field(name="**Andrew ❤️ Aidan**",
+704	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *Quoth Aidan; “Andrew grabbed my nuts once.”*",
+705	-                            inline=False)
+706	-         embedVar.add_field(name="**Aidan ❤️ Ethan**",
+707	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *Aidan once stuck his [parts] over Ethan's mouth while he was lying down...*",
+708	-                            inline=False)
+709	-         embedVar.add_field(name="**Ethan ❤️ Roan**",
+710	-                            value="Aliases: *There are no known aliases for this relationship.* \n Additional info: *Quoth Aidan; “Ethan once stuck his [parts] over Roan's mouth while he was lying down”...*",
+711	-                            inline=False)
+712	-         embedVar.add_field(name="**Anonymous B..h-person ❤️ Anonymous J..e-person**",
